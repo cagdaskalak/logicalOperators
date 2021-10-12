0bbae365ad0e1a08ca403a634f9e7935f00422e8
cagdaskalak <64699562+cagdaskalak@users.noreply.github.com>
2021-10-12 12:12:48 +0300

    Add files via upload

---
+++ logicalOperatorsNOT.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+public class logicalOperatorsNOT {
+    public static void main(String[] args) throws Exception {
+    
+     // logical operators = used to connect two or more expressions
+        
+                 //      && = (AND) both conditions must be true
+                 //      || = (OR) either condition must be true
+                 //      ! = (NOT) reverses boolean value of condition
+
+
+        
+                 Scanner scanner = new Scanner(System.in);
+
+                 System.out.println("You are playing a game! If you want to quit press q or Q");
+
+                 String response = scanner.next();
+
+                 if(!response.equals("q") && !response.equals("Q")){
+                     
+                     System.out.println("You still playing the game!");
+                 }
+                 else {
+                    System.out.println("You quit the game!");
+                 }
+                 scanner.close();
+        
+
+    
+    }
+
+}
